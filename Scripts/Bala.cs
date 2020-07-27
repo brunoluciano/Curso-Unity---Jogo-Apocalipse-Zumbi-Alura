0691b2df5bb978b4c3b6754119a7534e9e2c7654
brunoluciano <bruno.lucian0544@gmail.com>
2020-07-26 22:48:08 -0300

    Curso Parte 1 - Finalizado

---
+++ Scripts/Bala.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float Velocidade;
+    private Rigidbody rigidbodyBala;
+
+    void Start()
+    {
+        rigidbodyBala = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbodyBala.MovePosition
+            (rigidbodyBala.position + transform.forward * Velocidade * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider objetoDeColisao)
+    {
+        if(objetoDeColisao.tag == "Inimigo")
+        {
+            Destroy(objetoDeColisao.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+}
