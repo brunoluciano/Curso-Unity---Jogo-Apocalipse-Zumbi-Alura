ef462e6fc66895b60863d1a8dfbd45bb65225e9d
Bruno Luciano <bruno.lucian0544@gmail.com>
2020-07-28 15:55:24 -0300

    Finalizado até Aula 2 - Capítulo 10

--- Scripts/ControlaArma.cs
+++ Scripts/ControlaArma.cs
@@ -6,15 +6,15 @@ public class ControlaArma : MonoBehaviour
 {
     public GameObject Bala;
     public GameObject CanoDaArma;
-    public GameObject CanoDaArma1;
+    public AudioClip SomDeTiro;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            ControlaAudio.instancia.PlayOneShot(SomDeTiro);
             Instantiate(Bala, CanoDaArma.transform.position, CanoDaArma.transform.rotation);
-            Instantiate(Bala, CanoDaArma1.transform.position, CanoDaArma1.transform.rotation);
         }
     }
 }
